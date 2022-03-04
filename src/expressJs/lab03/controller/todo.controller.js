31a90c1fefdd85acb1571b15922517a93b7561dc
Tran Luan <tranluanqqq@gmail.com>
2022-03-04 11:35:44 +0700

    lab03

--- src/expressJs/lab03/controller/todo.controller.js
+++ src/expressJs/lab03/controller/todo.controller.js
@@ -1,23 +1,5 @@
 let uuid = require('short-uuid')
 const todoModel = require('../models/todo.model')
-let arrTodo = [
-    {
-        id: uuid.generate(),
-        name: "To do 1",
-        status: "New",
-    },
-    {
-        id: uuid.generate(),
-        name: "To do 2",
-        status: "New",
-    },
-    {
-        id: uuid.generate(),
-        name: "To do 3",
-        status: "New",
-    } 
-]
-
 module.exports = {
     getAllTodo : (req, res) => {
         todoModel.getAllToDo((result) => {
@@ -29,39 +11,33 @@ module.exports = {
     addTodo : (req, res) => {
         const {name} = req.body
         if(name && name.length > 0) {
-            let todo = {
-                id: uuid.generate(),
-                name: name,
-                status: "New"
-            }
-            arrTodo.push(todo)
-            return res.status(201).json({message: "Add to complete", todo: todo})
+            todoModel.addTodo(uuid.generate(),name, (err) => {
+                if(err) {
+                    return res.status(500).json({message: "Please enter a name"})
+                }
+                return res.status(201).json({message: "Add to complete"})
+            })
         }
-        return res.status(400).json({message: "Please enter a name"})
+        // return res.status(400).json({message: "Please enter a name"})
     },
 
     assignTodo : (req, res) => {
         const id = req.params.id
-        let index = arrTodo.findIndex(todo => todo.id === id)
-        if(index < 0) {
-            return res.status(404).json({message: "Not found"})
-        }
-        if(arrTodo[index].status === "Complete") {
-            return res.status(400).json({message: "Status todo completed"})
-        }
-
-        arrTodo[index].status = "Complete"
-        return res.status(200).json({message: "Assign to done", todo: arrTodo[index]})
+        todoModel.assignTodo(id, (err) => {
+            if(err) {
+                return res.status(500).json({message: "Server error"})
+            }
+            return res.status(201).json({message: "Assign complete"})
+        })
     },
 
     deleteTodo : (req, res) => {
         const id = req.params.id
-        let index = arrTodo.findIndex(todo => todo.id === id)
-        if(index < 0) {
-            return res.status(404).json({message: "Not found"})
-        }
-
-        arrTodo.splice(index, 1)
-        return res.status(200).json({message: "Delete todo complete"})
+        todoModel.deleteTodo(id, (err) => {
+            if(err) {
+                return res.status(500).json({message: "Server error"})
+            }
+            return res.status(201).json({message: "Delete complete"})
+        })
     }
 }
--- src/expressJs/lab03/models/todo.model.js
+++ src/expressJs/lab03/models/todo.model.js
@@ -6,5 +6,30 @@ module.exports = {
         connection.execute(query, (err, results) => {
             callback(results)
         })
+    },
+
+    addTodo: (id,name,callback) => {
+        let query = 'INSERT INTO todo(id,name) values (?,?)';
+        connection.query(query,[id,name], (err, results) => {
+            if (err) return callback(err, null);
+            return callback(null,results)
+        })
+
+    },
+
+    deleteTodo: (id, callback) => {
+        let query = 'DELETE FROM todo WHERE id = ?';
+        connection.query(query,[id], (err, results) => {
+            if(err) return callback(err, null);
+            return callback(null,results)
+        })
+    },
+
+    assignTodo: (id, callback) => {
+        let query = "UPDATE todo SET status = 'Completed' WHERE id = ?";
+        connection.query(query,[id], (err, results) => {
+            if(err) return callback(err, null);
+            return callback(null,results)
+        })
     }
 }
